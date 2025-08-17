b0ee27765a9cb04ad48d5fbdd0bda929bbd199ba
Nick Edell <various@humble2.com>
2025-08-17 17:36:38 +0100

    Adds a dynamic year to the footer
    Adds a Nunjucks filter to dynamically generate the current year.
    This allows the copyright notice in the footer to be updated automatically.
    Removes the hardcoded fallback year.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
-// .eleventy.js
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  eleventyConfig.addNunjucksFilter("year", (date = new Date()) =>
+	new Intl.DateTimeFormat("en", { year: "numeric" }).format(date)
+  );
+
   return {
 	dir: { input: "src", includes: "_includes", output: "_site" },
 	markdownTemplateEngine: "njk",
-	htmlTemplateEngine: "njk"
+	htmlTemplateEngine: "njk",
   };
 };
